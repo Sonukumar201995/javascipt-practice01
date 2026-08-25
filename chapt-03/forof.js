a23a7c5ad103d22f5aab0396207f3ea232f6a7e2
Sonu Kumar <krsonu025@gmail.com>
2026-08-25 14:13:10 +0530

    function and parameter function in js

--- chapt-03/forof.js
+++ chapt-03/forof.js
@@ -3,3 +3,10 @@ let fruits = ["Apple", "Banana", "Mango"];
 for (let fruit of fruits) {
     console.log(fruit);
 }
+
+
+let fruit=["apple","banana","orange"];
+for(let index in fruit)
+{
+    console.log(index,fruit[index])
+}
---
+++ chapt-03/parafun.js
@@ -0,0 +1,6 @@
+function greet(name)
+{
+    console.log("name "+name);
+}
+greet("sonu");
+greet("kumar");
