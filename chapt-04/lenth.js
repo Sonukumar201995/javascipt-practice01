2fd8bac242ac6d79c1639ea7a876b3331102cd7c
Sonu Kumar <krsonu025@gmail.com>
2026-08-26 14:01:59 +0530

    startWith method

--- chapt-04/lenth.js
+++ chapt-04/lenth.js
@@ -19,3 +19,10 @@ console.log(str[0])
 console.log(str[1])
 console.log(str[2])
 console.log(str[3])
+
+//includes
+console.log(str.includes("j"))
+console.log(str.includes("y"))
+
+// startWith
+console.log(str.startsWith("jav"))
