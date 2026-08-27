543e79b200bc73a662324cd1614f891531fd58ed
Sonu Kumar <krsonu025@gmail.com>
2026-08-27 15:23:48 +0530

    other methods

--- chapt-05/methods.js
+++ chapt-05/methods.js
@@ -1,2 +1,15 @@
 let arr=[10,20,30,40];
 console.log(arr.length)
+
+arr.push(50);
+console.log(arr);
+
+console.log(arr.pop())
+console.log(arr);
+
+console.log(arr.unshift(3))
+console.log(arr);
+
+console.log(arr.shift());
+console.log(arr);
+
