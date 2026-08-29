f27a8b2ce3f330c4278cb39ca95bda472c17fa67
Sonu Kumar <krsonu025@gmail.com>
2026-08-29 13:02:35 +0530

    walking the dom

---
+++ chapt-07/dom.js
@@ -0,0 +1,11 @@
+let body=document.body;
+console.log(body);
+
+let title=document.title;
+console.log(title)
+
+let head=document.head;
+console.log(head);
+
+let element=document.documentElement;
+console.log(element);
