7ba3317f78780094f9472ddcc9171cbc40d25659
Sonu Kumar <krsonu025@gmail.com>
2026-08-30 13:20:17 +0530

    closest methods

--- chapt-07/dom.js
+++ chapt-07/dom.js
@@ -1,4 +1,7 @@
+let id1 = document.getElementById("id1");
 
-let id1=document.getElementById("id1");
-console.log(id1.matches('.box1'))
+console.log(id1.matches(".box1"));
 
+let sp1 = document.getElementById("sp1");
+
+console.log(sp1.closest(".box1"));
