529c6e643183b09cdaa1c7302fb45ace8161a27e
Sonu Kumar <krsonu025@gmail.com>
2026-08-30 12:52:32 +0530

    searching the dom methods

--- chapt-07/dom.js
+++ chapt-07/dom.js
@@ -1,6 +1,49 @@
-let t=document.body
-console.log(t.firstElementChild);
+// 1. getElementById()
+const heading = document.getElementById("heading");
 
-let tab=document.body
-console.log(tab.firstElementChild);
-console.log(tab.rows)
+heading.innerText = "Welcome to JavaScript";
+heading.style.color = "red";
+
+
+// 2. getElementsByClassName()
+const texts = document.getElementsByClassName("text");
+
+texts[0].innerText = "Changed First Paragraph";
+texts[1].style.color = "blue";
+
+
+// 3. getElementsByTagName()
+const boxes = document.getElementsByTagName("div");
+
+boxes[0].style.backgroundColor = "yellow";
+boxes[1].style.backgroundColor = "green";
+
+
+// 4. getElementsByName()
+const username = document.getElementsByName("username");
+
+username[0].value = "Sonu";
+
+
+// 5. querySelector()
+const firstBox = document.querySelector(".box");
+
+firstBox.innerText = "First Box Changed";
+
+
+// 6. querySelectorAll()
+const allBoxes = document.querySelectorAll(".box");
+
+allBoxes.forEach((box) => {
+    box.style.padding = "20px";
+    box.style.margin = "10px";
+});
+
+
+// 7. Event / Behavior Change
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", () => {
+    heading.innerText = "Button Clicked!";
+    heading.style.color = "green";
+});
