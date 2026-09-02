501e216e62a6a3f822a089c6a37d52a004a76a12
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-02 13:36:48 +0530

    custom data

--- chapt-08/javascript.js
+++ chapt-08/javascript.js
@@ -1,12 +1,17 @@
+// let first=document.getElementById("id1");
+// let a=first.getAttribute("class");
+// console.log(a);
+
+// let b=first.getAttribute("id");
+// console.log(b);
+
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("style"));
+
+// console.log(first.setAttribute("class","sonu"))
+// console.log(first.removeAttribute("class"))
+
 let first=document.getElementById("id1");
-let a=first.getAttribute("class");
-console.log(a);
-
-let b=first.getAttribute("id");
-console.log(b);
-
-console.log(first.hasAttribute("class"));
-console.log(first.hasAttribute("style"));
-
-console.log(first.setAttribute("class","sonu"))
-console.log(first.removeAttribute("class"))
+console.log(first.dataset);
+console.log(first.dataset.game);
+console.log(first.dataset.player)
