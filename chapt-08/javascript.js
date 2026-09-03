eeeebf71f841d3f3897cf113f34a3fc4455dfad1
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-03 13:53:30 +0530

    handling browser events

--- chapt-08/javascript.js
+++ chapt-08/javascript.js
@@ -1,4 +1,19 @@
-function sayHello() {
-console.log("Button clicked!")
+let btn = document.getElementById("btn");
+
+let x = function (e) {
+    alert("hello 1");
+};
+
+let y = function (e) {
+    alert("hello 2");
+};
+
+// Event attach karo
+btn.addEventListener("click", x);
+btn.addEventListener("click", y);
+
+let a = prompt("Enter your number");
+
+if (a == "2") {
+    btn.removeEventListener("click", y);
 }
-sayHello();
