0e98b72147dfa7c672c0ebd466daa0d18517d77e
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-03 11:43:31 +0530

    set interval

---
+++ chapt-08/setInterval.js
@@ -0,0 +1,11 @@
+const user={
+    name:"sonu",
+    age:31,
+    address:"new delhi",
+    email:"sonu@gmail.com"
+}
+
+console.log("print data for user")
+setInterval(()=>{
+    console.log(user);
+},2000);
