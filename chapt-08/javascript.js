b2c9d42d42802b72ee6e034c0e963c68a5920743
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-03 12:05:57 +0530

    browser events

--- chapt-08/javascript.js
+++ chapt-08/javascript.js
@@ -1,8 +1,4 @@
-let first = document.getElementById("first");
-
-first.classList.add("red");
-first.classList.remove("red");
-first.classList.toggle("red");
-console.log(first.classList.contains("red"));
-first.classList.replace("red", "blue");
-first.className = "box blue";
+function sayHello() {
+console.log("Button clicked!")
+}
+sayHello();
