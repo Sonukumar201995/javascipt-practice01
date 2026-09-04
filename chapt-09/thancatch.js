f96df85204e525f3d9cc970278570c68d2ed1ed3
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-04 14:33:12 +0530

    then and catch in promise

--- chapt-09/thancatch.js
+++ chapt-09/thancatch.js
@@ -1,10 +1,27 @@
-let p=new Promise((resolve,reject)=>{
+let p1=new Promise((resolve,reject)=>{
     console.log("promise is pending");
     setTimeout(() => {
-        console.log("i am promise and fulfilled")
-        // resolve(true);
-        reject(new Error("i am a error"))
+        console.log("i am promise and resolve")
+        resolve(true);
+        
     }, 5000);
 })
 
-console.log(p)
+
+let p2=new Promise((resolve,reject)=>{
+    console.log("promise is panding");
+    setTimeout(() => {
+        console.log("a am promise and rejected")
+        reject(new Error("something wend wrong"))
+    }, 5000);
+})
+
+// console.log(p1,p2)
+
+p1.then((value)=>{
+    console.log(value)
+})
+
+p2.catch((error)=>{
+    console.log("some error occurrenc")
+})
