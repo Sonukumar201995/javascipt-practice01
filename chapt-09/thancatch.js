550eddb790c1f19bc2611f3c36795a304149f208
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-04 14:20:23 +0530

    promise reject

--- chapt-09/thancatch.js
+++ chapt-09/thancatch.js
@@ -2,7 +2,8 @@ let p=new Promise((resolve,reject)=>{
     console.log("promise is pending");
     setTimeout(() => {
         console.log("i am promise and fulfilled")
-        resolve(true);
+        // resolve(true);
+        reject(new Error("i am a error"))
     }, 5000);
 })
 
