6e3afeb3a65fd1b0d9363cdd872e7c4e4d49aef0
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-05 12:56:10 +0530

    resolve promise

--- chapt-09/promiseall.js
+++ chapt-09/promiseall.js
@@ -1,25 +1,11 @@
-let p1 = new Promise((res, rej) => {
-    setTimeout(() => {
-        rej("Error 1");
-    }, 1000);
-});
+let p=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(true)
+    },1000)
+})
 
-let p2 = new Promise((res, rej) => {
-    setTimeout(() => {
-        res("Value 2");
-    }, 2000);
-});
+let res=Promise.resolve(6);
+res.then((val)=>{
+    console.log(val);
+})
 
-let p3 = new Promise((res, rej) => {
-    setTimeout(() => {
-        res("Value 3");
-    }, 3000);
-});
-
-let res=Promise.any([p1, p2, p3])
-    res.then((value) => {
-        console.log(value);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
