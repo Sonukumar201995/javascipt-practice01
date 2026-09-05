f29bcea7cb078caab8d4cd1c04d7113867f4cc29
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-05 12:54:21 +0530

    any method

--- chapt-09/promiseall.js
+++ chapt-09/promiseall.js
@@ -1,38 +1,25 @@
-let p1=new Promise((res,rej)=>{
-    setTimeout(()=>{
-      res("value 1")  
-    },1000)
-})
+let p1 = new Promise((res, rej) => {
+    setTimeout(() => {
+        rej("Error 1");
+    }, 1000);
+});
 
-let p2=new Promise((res,rej)=>{
-    setTimeout(()=>{
-        res("value 2")
-    },2000)
-})
+let p2 = new Promise((res, rej) => {
+    setTimeout(() => {
+        res("Value 2");
+    }, 2000);
+});
 
-let p3=new Promise((res,rej)=>{
-    setTimeout(()=>{
-        res("value 3")
-    },3000)
-})
+let p3 = new Promise((res, rej) => {
+    setTimeout(() => {
+        res("Value 3");
+    }, 3000);
+});
 
-
-p1.then((val)=>{
-    console.log(val)
-})
-
-p2.then((val)=>{
-    console.log(val)
-})
-
-p3.then((val)=>{
-    console.log(val);
-})
-// promise all methods
-
-let promise=Promise.race([p1,p2,p3]);
-console.log(promise);
-
-promise.then((val)=>{
-    console.log("first resolve",val);
-})
+let res=Promise.any([p1, p2, p3])
+    res.then((value) => {
+        console.log(value);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
