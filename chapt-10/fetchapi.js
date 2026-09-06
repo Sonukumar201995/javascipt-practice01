38ae888154095a3c95b4bb9ec778b220b55d4569
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-06 14:15:02 +0530

    fetch api with then

--- chapt-10/fetchapi.js
+++ chapt-10/fetchapi.js
@@ -1,6 +1,4 @@
 let a=fetch("https://jsonplaceholder.typicode.com/users")
 a.then((value)=>{
-    console.log(value.status)
-    console.log(value.ok)
     console.log(value)
 })
---
+++ chapt-10/fetchapiwiththen.js
@@ -0,0 +1,12 @@
+let response=fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>{
+    return response.json();
+})
+
+.then((data)=>{
+    console.log(data)
+})
+
+.catch((error)=>{
+    console.log(error)
+})
