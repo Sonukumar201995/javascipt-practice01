b6b8741b4c7d9421b5d9bc6621a49b4a8a2fbf09
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-13 14:17:54 +0530

    match RegEx in javascript

--- chapt-12/regixex.js
+++ chapt-12/regixex.js
@@ -1,4 +1,16 @@
-let regex=/good/g;
-let text="sonu is good boy.and good communication skill";
+// let regex=/good/g;
+// let text="sonu is good boy.and good communication skill";
 
-console.log(text.replace(regex,"GOOD"));
+// console.log(text.replace(regex,"GOOD"));
+
+
+// test
+const regex = /hello/i;
+
+console.log(regex.test("Hello World")); // true
+
+// match.
+
+const text = "My age is 22";
+
+console.log(text.match(/\d+/g));
