cca91fad1bf1678f031b405e431e0fe47b6a0907
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-14 18:32:39 +0530

    javascript oop basic examle

---
+++ chapt-13 oop/example.js
@@ -0,0 +1,10 @@
+class person{
+    constructor(name,age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+}
+const person1=new person("sonu",31);
+console.log(person1.name+"->"+person1.age)
