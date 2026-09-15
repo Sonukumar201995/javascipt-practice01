f21da2fccaf69380016f8d8d34b73455586f91b1
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-15 12:52:10 +0530

    class and object

---
+++ chapt-13 oop/classobject.js
@@ -0,0 +1,11 @@
+class student{
+    constructor(name,age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+}
+
+let s1=new student("sonu",31);
+console.log(s1.name);
+console.log(s1.age);
