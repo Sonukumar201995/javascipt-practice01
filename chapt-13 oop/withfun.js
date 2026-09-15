9c934c4b4cbeabb3a806231c8d458ec1b4ea597f
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-15 12:56:19 +0530

    with using function in class object

---
+++ chapt-13 oop/withfun.js
@@ -0,0 +1,15 @@
+class student{
+    study()
+    {
+        console.log("student is studying")
+    }
+
+    play()
+    {
+        console.log("student is playing")
+    }
+}
+
+let s=new student();
+s.study();
+s.play();
