c662c962f28c4d99572dd320a86a81199bc36fd5
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-15 14:50:01 +0530

    perameterized constructor

---
+++ chapt-13 oop/parameterizedconstructor.js
@@ -0,0 +1,13 @@
+class student{
+    constructor(name,age,address)
+    {
+        this.name=name;
+        this.age=age;
+        this.address=address;
+    }
+}
+
+let s=new student("sonu",31,"new delhi");
+console.log(s.name)
+console.log(s.age)
+console.log(s.address)
