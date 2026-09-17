ab2a79fea3c564f6cf6e60771bac6729a66931df
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-17 12:32:26 +0530

    super keyword in js

---
+++ chapt-13 oop/super.js
@@ -0,0 +1,16 @@
+
+class Animal {
+    sound() {
+        console.log("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    sound() {
+        super.sound();   // calls parent method
+        console.log("Dog barks");
+    }
+}
+
+const dog = new Dog();
+dog.sound();
